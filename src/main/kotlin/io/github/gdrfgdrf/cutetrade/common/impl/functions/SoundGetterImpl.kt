03afc36aad7f3d18d3dbf976b8d089771745dd5b
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-08-08 17:36:13 +0800

    change the symbol of sound events

--- src/main/kotlin/io/github/gdrfgdrf/cutetrade/common/impl/functions/SoundGetterImpl.kt
+++ src/main/kotlin/io/github/gdrfgdrf/cutetrade/common/impl/functions/SoundGetterImpl.kt
@@ -6,12 +6,12 @@ import net.minecraft.sound.SoundEvent
 import net.minecraft.sound.SoundEvents
 
 object SoundGetterImpl : CommonFunctions.SoundGetter {
-    val START_SOUND = SoundEvent.of(createIdentifier("minecraft:entity.experience_orb.pickup"))
-    val STATE_POSITIVE_SOUND = SoundEvent.of(createIdentifier("minecraft:block.note_block.harp"))
-    val STATE_NEGATIVE_SOUND = SoundEvent.of(createIdentifier("minecraft:block.note_block.bass"))
-    val ADD_ITEM_SOUND = SoundEvent.of(createIdentifier("minecraft:block.note_block.bell"))
-    val REMOVE_ITEM_SOUND = SoundEvent.of(createIdentifier("minecraft:block.note_block.didgeridoo"))
-    val TERMINATE_SOUND = SoundEvent.of(createIdentifier("minecraft:block.anvil.land"))
+    val START_SOUND = SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP
+    val STATE_POSITIVE_SOUND = SoundEvents.BLOCK_NOTE_BLOCK_HARP
+    val STATE_NEGATIVE_SOUND = SoundEvents.BLOCK_NOTE_BLOCK_BASS
+    val ADD_ITEM_SOUND = SoundEvents.BLOCK_NOTE_BLOCK_BELL
+    val REMOVE_ITEM_SOUND = SoundEvents.BLOCK_NOTE_BLOCK_DIDGERIDOO
+    val TERMINATE_SOUND = SoundEvents.BLOCK_ANVIL_LAND
     val FINISH_SOUND = SoundEvents.ENTITY_PLAYER_LEVELUP
 
     override fun getTradeStartSound(): Any = START_SOUND
